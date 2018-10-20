895151740c181445fd347221faad93e2c5c80c3f
TaikenS <shintani.taiken@irl.sys.es.osaka-u.ac.jp>
2018-10-20 19:24:26 +0900

    add user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_20_083127) do
+ActiveRecord::Schema.define(version: 2018_10_20_084556) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "room_id"
+    t.string "attachment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
