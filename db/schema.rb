453a438561761816bfc2e7b438e5ddded88085d4
TaikenS <shintani.taiken@irl.sys.es.osaka-u.ac.jp>
2018-10-21 01:03:41 +0900

    stable

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_20_104030) do
+ActiveRecord::Schema.define(version: 2018_10_20_155920) do
 
   create_table "histories", force: :cascade do |t|
-    t.boolean "inout"
+    t.boolean "in_or_out"
     t.string "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
